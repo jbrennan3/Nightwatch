f5983ea95e8ade88eac759fe963e3580e4b0e590
Brennan <jeremb@gmail.com>
2016-06-06 01:50:22 -0700

    fixed lasergun bug

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -6,16 +6,16 @@ using UnitySampleAssets.CrossPlatformInput;
 
 public class PlayerShooting : MonoBehaviour
 {
-	
-    //public int damagePerShot = 20;                  // The damage inflicted by each bullet.
-    //public float timeBetweenBullets = 0.15f;        // The time between each shot.
-    //public float range = 100f;                      // The distance the gun can fire.
+
+	//public int damagePerShot = 20;                  // The damage inflicted by each bullet.
+	//public float timeBetweenBullets = 0.15f;        // The time between each shot.
+	//public float range = 100f;                      // The distance the gun can fire.
 
 
-    float timer = 0f;                               // A timer to determine when to fire.
+	float timer = 0f;                               // A timer to determine when to fire.
 	float laserCD = 11f;							// Cool Down timer for OP laser cannon. Set to > 10 so player can shoot on spawn.
 
-    Ray shootRay;                                   // A ray from the gun end forwards.
+	Ray shootRay;                                   // A ray from the gun end forwards.
 	Ray shootLeft;
 	Ray shootRight;
 	public LineRenderer leftline;
@@ -27,24 +27,24 @@ public class PlayerShooting : MonoBehaviour
 	public Toggle Lasertoggle;
 	public Text LaserCDtext;
 
-    RaycastHit shootHit;                            // A raycast hit to get information about what was hit.
-    int shootableMask;                              // A layer mask so the raycast only hits things on the shootable layer.
-    ParticleSystem gunParticles;                    // Reference to the particle system.
-    LineRenderer gunLine;                           // Reference to the line renderer.
-    AudioSource[] gunShotClips;
-    Light gunLight;                                 // Reference to the light component.
+	RaycastHit shootHit;                            // A raycast hit to get information about what was hit.
+	int shootableMask;                              // A layer mask so the raycast only hits things on the shootable layer.
+	ParticleSystem gunParticles;                    // Reference to the particle system.
+	LineRenderer gunLine;                           // Reference to the line renderer.
+	AudioSource[] gunShotClips;
+	Light gunLight;                                 // Reference to the light component.
 	public Light faceLight;								// Duh
-    float effectsDisplayTime = 0.2f;                // The proportion of the timeBetweenBullets that the effects will display for.
+	float effectsDisplayTime = 0.2f;                // The proportion of the timeBetweenBullets that the effects will display for.
 
-    private enum Gun { Rifle, Shotgun, Laser };
+	private enum Gun { Rifle, Shotgun, Laser };
 
-    Gun gunType;
-    private int currentDamage;
+	Gun gunType;
+	private int currentDamage;
 	private float currentTime;
 	private float currentRange;
 	private float interval = 0f;							//for text update
 
-    class GunClass {
+	class GunClass {
 		public int damagePerShot;
 		public float timeBetweenBullets;
 		public float range;
@@ -54,38 +54,38 @@ public class PlayerShooting : MonoBehaviour
 		public GunClass (int _damage, float _time, float _range, AudioSource _audio, float accuracy) {
 			damagePerShot = _damage;
 			timeBetweenBullets = _time;
-    		range = _range;
+			range = _range;
 			gunShotClip = _audio;
 			this.accuracy = accuracy; 
-    	}
-    }
+		}
+	}
 
 	GunClass[] gunList = new GunClass[3];
 
-    void Awake ()
-    {
-        // Create a layer mask for the Shootable layer.
-        shootableMask = LayerMask.GetMask ("Shootable");
+	void Awake ()
+	{
+		// Create a layer mask for the Shootable layer.
+		shootableMask = LayerMask.GetMask ("Shootable");
 
-        // Set up the references.
-        gunParticles = GetComponent<ParticleSystem> ();
-        gunLine = GetComponent <LineRenderer> ();
-        gunShotClips = GetComponents<AudioSource> ();
-        gunLight = GetComponent<Light> ();
+		// Set up the references.
+		gunParticles = GetComponent<ParticleSystem> ();
+		gunLine = GetComponent <LineRenderer> ();
+		gunShotClips = GetComponents<AudioSource> ();
+		gunLight = GetComponent<Light> ();
 		//faceLight = GetComponentInChildren<Light> ();
 
 		gunShotClips[2].volume = 0.8f;
 
 		gunList [0] = new GunClass (20, 0.15f, 32f, gunShotClips[0], 0.8f); // Rifle
 		gunList [1] = new GunClass (100, 0.8f, 10f, gunShotClips[2], 0.2f); // Shotgun
-		gunList [2] = new GunClass (200, 5f, 50f, gunShotClips[1], 1.1f); // Laser
+		gunList [2] = new GunClass (200, 1.5f, 50f, gunShotClips[1], 1.1f); // Laser
 		gunType = Gun.Rifle;
-    }
+	}
 
-    void Update ()
-    {
-        // Add the time since Update was last called to the timer.
-        timer += Time.deltaTime;
+	void Update ()
+	{
+		// Add the time since Update was last called to the timer.
+		timer += Time.deltaTime;
 		laserCD += Time.deltaTime;
 		interval += Time.deltaTime;
 		if (laserCD < 10f){
@@ -99,27 +99,28 @@ public class PlayerShooting : MonoBehaviour
 			LaserCDtext.text = "Laser Cooldown: READY";
 		}
 
-#if !MOBILE_INPUT
-        // If the Fire1 button is being press and it's time to fire...
+		#if !MOBILE_INPUT
+		// If the Fire1 button is being press and it's time to fire...
 		if((Input.GetButton ("Fire1") || Input.GetKey("space")) && timer >= currentTime && Time.timeScale != 0)
-        {
-            // ... shoot the gun.
-            Shoot(gunType);
-        }
-#else
-        // If there is input on the shoot direction stick and it's time to fire...
-        if ((CrossPlatformInputManager.GetAxisRaw("Mouse X") != 0 || CrossPlatformInputManager.GetAxisRaw("Mouse Y") != 0) && timer >= timeBetweenBullets)
-        {
-            // ... shoot the gun
-            Shoot();
-        }
-#endif
-        // If the timer has exceeded the proportion of timeBetweenBullets that the effects should be displayed for...
-        if(timer >= currentTime * effectsDisplayTime)
-        {
-            // ... disable the effects.
-            DisableEffects ();
-        }
+		{
+			// ... shoot the gun.
+			Shoot(gunType);
+		}
+		#else
+		// If there is input on the shoot direction stick and it's time to fire...
+		if ((CrossPlatformInputManager.GetAxisRaw("Mouse X") != 0 || CrossPlatformInputManager.GetAxisRaw("Mouse Y") != 0) && timer >= timeBetweenBullets)
+		{
+		// ... shoot the gun
+		Shoot();
+		}
+		#endif
+		// If the timer has exceeded the proportion of timeBetweenBullets that the effects should be displayed for...
+		if (laserline.enabled == false && timer >= currentTime * effectsDisplayTime) {
+			// ... disable the effects.
+			DisableEffects ();
+		} else if (timer >= currentTime * 0.6f) {
+			DisableLaser ();
+		}
 
 		if (Input.GetKeyDown (KeyCode.Alpha1)) {
 			gunType = Gun.Rifle;
@@ -132,26 +133,28 @@ public class PlayerShooting : MonoBehaviour
 			AKtoggle.isOn = false;
 			Shotguntoggle.isOn = true;
 			Lasertoggle.isOn = false;
-        } 
+		} 
 		else if (Input.GetKeyDown (KeyCode.Alpha3)) {
 			gunType = Gun.Laser;
 			AKtoggle.isOn = false;
 			Shotguntoggle.isOn = false;
 			Lasertoggle.isOn = true;
 		}
-    }
+	}
 
-
-    public void DisableEffects ()
-    {
-        // Disable the line renderer and the light.
-        gunLine.enabled = false;
+	public void DisableLaser ()
+	{
+		laserline.enabled = false;
+	}
+	public void DisableEffects ()
+	{
+		// Disable the line renderer and the light.
+		gunLine.enabled = false;
 		leftline.enabled = false;
 		rightline.enabled = false;
 		faceLight.enabled = false;
-        gunLight.enabled = false;
-		laserline.enabled = false;
-    }
+		gunLight.enabled = false;
+	}
 
 	void ifHit(Ray shot, float currentRange, int currentDamage, LineRenderer outline) {
 		// Perform the raycast against gameobjects on the shootable layer and if it hits something...
@@ -191,27 +194,27 @@ public class PlayerShooting : MonoBehaviour
 	}
 
 	void Shoot(Gun gunType)
-    {
-        currentDamage = gunList[(int)gunType].damagePerShot;
-        currentTime = gunList[(int)gunType].timeBetweenBullets;
-        currentRange = gunList[(int)gunType].range;
+	{
+		currentDamage = gunList[(int)gunType].damagePerShot;
+		currentTime = gunList[(int)gunType].timeBetweenBullets;
+		currentRange = gunList[(int)gunType].range;
 
 		if (gunType != Gun.Laser || (gunType == Gun.Laser && laserCD > 10.0f)) {
-	        // Reset the timer.
-	        timer = 0f;
+			// Reset the timer.
+			timer = 0f;
 
-	        // Play the gun shot audioclip.
-	        gunList[(int)gunType].gunShotClip.Play();
+			// Play the gun shot audioclip.
+			gunList[(int)gunType].gunShotClip.Play();
 
-	        // Enable the lights.
-	        gunLight.enabled = true;
-	        faceLight.enabled = true;
+			// Enable the lights.
+			gunLight.enabled = true;
+			faceLight.enabled = true;
 
-	        // Stop the particles from playing if they were, then start the particles.
-	        gunParticles.Stop();
-	        gunParticles.Play();
+			// Stop the particles from playing if they were, then start the particles.
+			gunParticles.Stop();
+			gunParticles.Play();
 
-	        // Enable the line renderer and set it's first position to be the end of the gun.
+			// Enable the line renderer and set it's first position to be the end of the gun.
 			if (gunType != Gun.Laser) {
 				gunLine.enabled = true;
 				gunLine.SetPosition (0, transform.position);
@@ -219,10 +222,10 @@ public class PlayerShooting : MonoBehaviour
 				laserline.enabled = true;
 				laserline.SetPosition (0, transform.position);
 			}
-	        // Set the shootRay so that it starts at the end of the gun and points forward from the barrel.
+			// Set the shootRay so that it starts at the end of the gun and points forward from the barrel.
 
 			shootRay.direction = transform.forward;
-	        shootRay.origin = transform.position;
+			shootRay.origin = transform.position;
 			shootLeft.origin = transform.position; //for shotgun
 			shootRight.origin = transform.position; //for shotgun
 
@@ -258,5 +261,5 @@ public class PlayerShooting : MonoBehaviour
 				laserline.SetPosition (1, shootRay.origin + shootRay.direction * currentRange);
 			}
 		}
-    }
+	}
 }
